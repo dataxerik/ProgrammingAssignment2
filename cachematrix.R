3683c6e640363449efcc4ace7217bb28f12d4f19
Xerik <dsshar@gmail.com>
2016-06-09 00:36:30 -0400

    Misread problem statmeent
    I thought we should take the transpose of the matrix. It is actually the
    inverse of the matrix. I've updated the code to reflect that.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## This function takes a matrix and has functions to cache and return the transpose of that matrix
+## This function takes a matrix and has functions to cache and return the inverse of that matrix
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -10,26 +10,26 @@ makeCacheMatrix <- function(x = matrix()) {
         m <<- NULL
     }
     get <- function() x
-    setTranspose <- function(t) m <<- t
-    getTranspose <- function() m
+    setInverse <- function(solve) m <<- solve
+    getInverse <- function() m
     list(set = set, get = get,
-         setTranspose = setTranspose,
-         getTranspose = getTranspose)
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## This will check to see if there is a stored transpose matrix, it not, then it will take the transpose of the matrix
-## and assign the transpose to m
+## This will check to see if there is a stored Inverse matrix, it not, then it will take the transpose of the matrix
+## and assign the Inverse to m
 
 cacheSolve <- function(x, ...) {
-    m <- x$getTranspose()
+    m <- x$getInverse()
     if(!is.null(m)) {
         message("getting cached data")
         return(m)
     }
     data <- x$get()
-    m <- t(data)
-    x$setTranspose(m)
+    m <- solve(data)
+    x$setInverse(m)
     m
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
 }
