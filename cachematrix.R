1ca9f9155550d74c4c6b61ecca66e1a8e88dd82f
Xerik <dsshar@gmail.com>
2016-06-08 23:46:42 -0400

    Modified file as per assignment requirements

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function takes a matrix and has functions to cache and return the transpose of that matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setTranspose <- function(t) m <<- t
+    getTranspose <- function() m
+    list(set = set, get = get,
+         setTranspose = setTranspose,
+         getTranspose = getTranspose)
 }
 
 
-## Write a short comment describing this function
+## This will check to see if there is a stored transpose matrix, it not, then it will take the transpose of the matrix
+## and assign the transpose to m
 
 cacheSolve <- function(x, ...) {
+    m <- x$getTranspose()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- t(data)
+    x$setTranspose(m)
+    m
         ## Return a matrix that is the inverse of 'x'
 }
